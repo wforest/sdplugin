6e152ce972d6521eceebc529cd50caa1a47880d7
Bill Sullivan <sulli219@gmail.com>
2017-06-30 09:02:29 -0400

    Initial Commit

---
+++ sd-plugin.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @package SD Plugin
+ */
+
+/*
+Plugin Name: SD Plug-in
+Plugin URI:
+Description: A brief description of the Plugin.
+Version: 0.1.0
+Author: Bill Sullivan
+Author URI: http://www.sullidigital.com
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: sdplugin
+Domain Path: /languages
+*/
+
+/* Copyright 2017 Bill Sullivan
+
+SD Plug-in is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+SD Plug-in is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SD Plug-in. If not, see {URI to Plugin License}.
+*/
