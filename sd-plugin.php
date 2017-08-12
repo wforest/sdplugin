679e462556c5930861e5ca0906b712b91559b27d
Bill Sullivan <sulli219@gmail.com>
2017-08-12 11:14:01 -0400

    Addition of Plan and Spec custom post type. Addition of plan type and plan status custom taxonomy for the plan post type. Addition of the Spec Status custom taxonomy for the spec post type

--- sd-plugin.php
+++ sd-plugin.php
@@ -116,6 +116,112 @@ function location_post_type() {
 add_action( 'init', 'location_post_type');
 
 /**
+ * Register a custom post type called "plan"
+ *
+ * @see get_post_type_labels() for label keys
+ */
+function plan_post_type() {
+    $labels = array(
+        'name'                  => _x( 'Plans', 'Post type general name', 'sdplugin' ),
+        'singular_name'         => _x( 'Plan', 'Post type singular name', 'sdplugin' ),
+        'menu_name'             => _x( 'Plans', 'Admin Menu text', 'sdplugin' ),
+        'name_admin_bar'        => _x( 'Plan', 'Add New on Toolbar', 'sdplugin' ),
+        'add_new'               => __( 'Add New', 'sdplugin' ),
+        'add_new_item'          => __( 'Add New Plan', 'sdplugin' ),
+        'new_item'              => __( 'New Plan', 'sdplugin' ),
+        'edit_item'             => __( 'Edit Plan', 'sdplugin' ),
+        'view_item'             => __( 'View Plan', 'sdplugin' ),
+        'all_items'             => __( 'All Plans', 'sdplugin' ),
+        'search_items'          => __( 'Search Plans', 'sdplugin' ),
+        'parent_item_colon'     => __( 'Parent Plans:', 'sdplugin' ),
+        'not_found'             => __( 'No plans found.', 'sdplugin' ),
+        'not_found_in_trash'    => __( 'No plans found in Trash.', 'sdplugin' ),
+        'featured_image'        => _x( 'Plan Image', 'sdplugin' ),
+        'set_featured_image'    => _x( 'Set plan image', 'sdplugin' ),
+        'remove_featured_image' => _x( 'Remove plan image', 'sdplugin' ),
+        'use_featured_image'    => _x( 'Use as plan image', 'sdplugin' ),
+        'archives'              => _x( 'Plans archives', 'sdplugin' ),
+        'insert_into_item'      => _x( 'Insert into plan', 'sdplugin' ),
+        'uploaded_to_this_item' => _x( 'Uploaded to this plans', 'sdplugin' ),
+        'filter_items_list'     => _x( 'Filter plans list', 'sdplugin' ),
+        'items_list_navigation' => _x( 'Plans list navigation', 'sdplugin' ),
+        'items_list'            => _x( 'Plans list', 'sdplugin' ),
+    );
+
+    $args = array(
+        'labels'                => $labels,
+        'public'                => true,
+        'publicly_queryable'    => true,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'query_var'             => true,
+        'rewrite'               => array( 'slug' => 'plan' ),
+        'capability_type'       => 'post',
+        'has_archive'           => true,
+        'hierarchical'          => false,
+        'menu_position'         => null,
+        'menu_icon'             => 'dashicons-portfolio',
+        'supports'              => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
+    );
+
+    register_post_type( 'plan', $args );
+}
+add_action( 'init', 'plan_post_type');
+
+/**
+ * Register a custom post type called "spec"
+ *
+ * @see get_post_type_labels() for label keys
+ */
+function spec_post_type() {
+    $labels = array(
+        'name'                  => _x( 'Specs', 'Post type general name', 'sdplugin' ),
+        'singular_name'         => _x( 'Spec', 'Post type singular name', 'sdplugin' ),
+        'menu_name'             => _x( 'Specs', 'Admin Menu text', 'sdplugin' ),
+        'name_admin_bar'        => _x( 'Spec', 'Add New on Toolbar', 'sdplugin' ),
+        'add_new'               => __( 'Add New', 'sdplugin' ),
+        'add_new_item'          => __( 'Add New Spec', 'sdplugin' ),
+        'new_item'              => __( 'New Spec', 'sdplugin' ),
+        'edit_item'             => __( 'Edit Spec', 'sdplugin' ),
+        'view_item'             => __( 'View Spec', 'sdplugin' ),
+        'all_items'             => __( 'All Specs', 'sdplugin' ),
+        'search_items'          => __( 'Search Specs', 'sdplugin' ),
+        'parent_item_colon'     => __( 'Parent Specs:', 'sdplugin' ),
+        'not_found'             => __( 'No specs found.', 'sdplugin' ),
+        'not_found_in_trash'    => __( 'No specs found in Trash.', 'sdplugin' ),
+        'featured_image'        => _x( 'Spec Image', 'sdplugin' ),
+        'set_featured_image'    => _x( 'Set spec image', 'sdplugin' ),
+        'remove_featured_image' => _x( 'Remove spec image', 'sdplugin' ),
+        'use_featured_image'    => _x( 'Use as spec image', 'sdplugin' ),
+        'archives'              => _x( 'Specs archives', 'sdplugin' ),
+        'insert_into_item'      => _x( 'Insert into spec', 'sdplugin' ),
+        'uploaded_to_this_item' => _x( 'Uploaded to this specs', 'sdplugin' ),
+        'filter_items_list'     => _x( 'Filter specs list', 'sdplugin' ),
+        'items_list_navigation' => _x( 'Specs list navigation', 'sdplugin' ),
+        'items_list'            => _x( 'Specs list', 'sdplugin' ),
+    );
+
+    $args = array(
+        'labels'                => $labels,
+        'public'                => true,
+        'publicly_queryable'    => true,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'query_var'             => true,
+        'rewrite'               => array( 'slug' => 'plan' ),
+        'capability_type'       => 'post',
+        'has_archive'           => true,
+        'hierarchical'          => false,
+        'menu_position'         => null,
+        'menu_icon'             => 'dashicons-media-spreadsheet',
+        'supports'              => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
+    );
+
+    register_post_type( 'spec', $args );
+}
+add_action( 'init', 'spec_post_type');
+
+/**
  * Register a 'Product Type' taxonomy for post type 'location'
  *
  * @see register_post_type for registering post types.
@@ -143,3 +249,47 @@ function create_sales_stage_tax() {
     ) );
 }
 add_action( 'init', 'create_sales_stage_tax', 0 );
+
+/**
+ * Register a 'Plan Type' taxonomy for post type 'plan'
+ *
+ * @see register_post_type for registering post types.
+ */
+function create_plan_type_tax() {
+    register_taxonomy( 'plan type', 'plan', array(
+        'label'         => __( 'Plan Type', 'sdplugin' ),
+        'rewrite'       => array( 'slug' => 'plans/plan_type' ),
+        'hierarchical'  => true,
+    ) );
+}
+add_action( 'init', 'create_plan_type_tax', 0 );
+
+/**
+ * Register a 'Plan Status' taxonomy for post type 'plan'
+ *
+ * @see register_post_type for registering post types.
+ */
+function create_plan_status_tax() {
+
+    register_taxonomy( 'plan status', 'plan', array(
+        'label'         => __( 'Plan Status', 'sdplugin' ),
+        'rewrite'       => array( 'slug' => 'plans/plan_status' ),
+        'hierarchical'  => false,
+    ) );
+}
+add_action( 'init', 'create_plan_status_tax', 0 );
+
+/**
+ * Register a 'Plan Status' taxonomy for post type 'plan'
+ *
+ * @see register_post_type for registering post types.
+ */
+function create_spec_status_tax() {
+
+    register_taxonomy( 'spec status', 'spec', array(
+        'label'         => __( 'Spec Status', 'sdplugin' ),
+        'rewrite'       => array( 'slug' => 'specs/spec_status' ),
+        'hierarchical'  => false,
+    ) );
+}
+add_action( 'init', 'create_spec_status_tax', 0 );
